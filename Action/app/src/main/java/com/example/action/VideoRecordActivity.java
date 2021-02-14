4b51ed491d9dc41a3ba6750078df14cf77848e34
Jungik Jang <51084152+cheeseBG@users.noreply.github.com>
2021-02-14 18:12:07 +0900

    Add activities
    - Analysis check activity
    - Video check activity
    - Video record activity
    + XML

---
+++ Action/app/src/main/java/com/example/action/VideoRecordActivity.java
@@ -0,0 +1,14 @@
+package com.example.action;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VideoRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_record);
+    }
+}
